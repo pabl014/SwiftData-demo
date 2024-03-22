6a603d9180c2b02669211ea80ef73015c400a772
Paweł Rudnik <pawelrudnik3@gmail.com>
2024-03-22 13:34:08 +0100

    Create container

--- SwiftDataDemo/SwiftDataDemoApp.swift
+++ SwiftDataDemo/SwiftDataDemoApp.swift
@@ -6,12 +6,23 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct SwiftDataDemoApp: App {
+    
+//    let container: ModelContainer = {
+//        let schema = Schema([Expense.self])
+//        //let config = ModelConfiguration(for: ...) // you can customize a container with all the config you need
+//        let container = try! ModelContainer(for: schema, configurations: [])
+//        return container
+//    }()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        //.modelContainer(container)
+        .modelContainer(for: [Expense.self]) // -> array, where you place all of your models ([Expense.self, ExpenseReport.self, User.self, ...])
     }
 }
